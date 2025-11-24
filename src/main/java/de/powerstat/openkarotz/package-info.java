3528d3b5b6f301fdc04f636197b02a0838f02fbe
Kai Hofmann <powerstat@web.de>
2025-11-24 16:18:27 +0100

    Added apache license headers

--- src/main/java/de/powerstat/openkarotz/package-info.java
+++ src/main/java/de/powerstat/openkarotz/package-info.java
@@ -1,4 +1,9 @@
 /**
+ * Copyright (C) 2021-2025 Dipl.-Inform. Kai Hofmann. All rights reserved!
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements; and to You under the Apache License, Version 2.0.
+ */
+
+/**
  * OpenKarotz api.
  */
 package de.powerstat.openkarotz;
